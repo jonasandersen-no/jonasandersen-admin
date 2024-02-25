6c11544f2a4daafee2a53628f26b28dc0df73c7c
Jonas Bjørge Andersen <jonas.bjorge.andersen@gmail.com>
2024-02-25 14:52:29 +0100

    Made IP record

---
+++ admin-core/src/main/java/no/jonasandersen/admin/core/minecraft/domain/Ip.java
@@ -0,0 +1,13 @@
+package no.jonasandersen.admin.core.minecraft.domain;
+
+public record Ip(String value) {
+
+  public static final String REGEX = "(\\b25[0-5]|\\b2[0-4][0-9]|\\b[01]?[0-9][0-9]?)(\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}";
+
+  public Ip {
+    if (!value.matches(REGEX)) {
+      throw new IllegalArgumentException("Invalid IP address");
+    }
+  }
+
+}
---
+++ admin-core/src/test/java/no/jonasandersen/admin/core/minecraft/domain/IpTest.java
@@ -0,0 +1,21 @@
+package no.jonasandersen.admin.core.minecraft.domain;
+
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.Test;
+
+class IpTest {
+
+  @Test
+  void ipAddressIsValidFormat() {
+    assertThatCode(() -> new Ip("127.0.0.1")).doesNotThrowAnyException();
+  }
+
+  @Test
+  void ipAddressIsInvalidFormat() {
+    assertThatThrownBy(() -> new Ip("1234.1234.1234.1234")).isInstanceOf(
+        IllegalArgumentException.class);
+  }
+}
