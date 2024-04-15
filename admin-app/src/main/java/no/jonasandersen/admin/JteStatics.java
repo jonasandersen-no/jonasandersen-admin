41be6951d0ce2662c5c6e3966929397a0add9a24
Jonas Bjørge Andersen <dev@jonasandersen.no>
2024-04-15 16:36:04 +0200

    Added build properties as static method

--- admin-app/src/main/java/no/jonasandersen/admin/JteStatics.java
+++ admin-app/src/main/java/no/jonasandersen/admin/JteStatics.java
@@ -4,21 +4,23 @@ import java.util.HashMap;
 import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.info.BuildProperties;
 import org.springframework.context.event.EventListener;
 import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 @Service
-public class ThemeService {
+public class JteStatics {
 
-  private static final Logger log = LoggerFactory.getLogger(ThemeService.class);
-  public static String theme = "light";
-
+  private static final Logger log = LoggerFactory.getLogger(JteStatics.class);
+  private static String theme = "light";
   private static final Map<Object, String> themes = new HashMap<>();
+  private static String buildVersion;
 
-  public ThemeService(AdminProperties adminProperties) {
+  JteStatics(AdminProperties adminProperties, BuildProperties buildProperties) {
     theme = adminProperties.defaultTheme();
+    buildVersion = buildProperties.getVersion();
   }
 
 
@@ -28,6 +30,11 @@ public class ThemeService {
 
     log.info("Setting theme to dark for user: {}", principal);
     themes.put(principal, "dark");
+  }
+
+  public static String getBuildVersion() {
+    log.debug("Getting version: {}", buildVersion);
+    return buildVersion;
   }
 
   public static String getTheme() {
