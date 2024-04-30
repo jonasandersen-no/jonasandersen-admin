0eb1d5ce50e64204b251df3099b5dcbbce60c7f9
Jonas Bjørge Andersen <jonas.bjorge.andersen@gmail.com>
2024-04-30 19:02:27 +0200

    Rewrote test

--- admin-app/src/test/java/no/jonasandersen/admin/adapter/in/web/shortcut/ShortcutControllerTest.java
+++ admin-app/src/test/java/no/jonasandersen/admin/adapter/in/web/shortcut/ShortcutControllerTest.java
@@ -1,30 +1,24 @@
 package no.jonasandersen.admin.adapter.in.web.shortcut;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 import no.jonasandersen.admin.config.IoBasedTest;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WithMockUser
-@Disabled
 class ShortcutControllerTest extends IoBasedTest {
 
   @Autowired
   private MockMvc mockMvc;
 
   @Test
-  void modelHaveShortcutsAttribute() throws Exception {
+  void shortcutPathReturnsOk() throws Exception {
     this.mockMvc
         .perform(get("/shortcut"))
-        .andExpect(status().isOk())
-        .andExpect(view().name("shortcut-overview"))
-        .andExpect(model().attributeExists("projectNames"));
+        .andExpect(status().isOk());
   }
 }
