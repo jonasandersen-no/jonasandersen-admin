3538ec19a59e1eea5da3065486c66935ff24ee92
Jonas Bjørge Andersen <jonas.bjorge.andersen@gmail.com>
2024-05-10 14:27:06 +0200

    Renamed controller io test

--- src/test/java/no/jonasandersen/admin/adapter/in/api/ServerGeneratorControllerIoTest.java
+++ src/test/java/no/jonasandersen/admin/adapter/in/api/ServerGeneratorControllerIoTest.java
@@ -12,7 +12,7 @@ import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WithMockUser
-class ServerGeneratorControllerTest extends IoBasedTest {
+class ServerGeneratorControllerIoTest extends IoBasedTest {
 
   @Autowired
   private MockMvc mockMvc;
