ab8754e208d5d331b0ed4b89df1777aa67ad493f
Jonas Bjørge Andersen <dev@jonasandersen.no>
2024-05-12 14:20:54 +0200

    Removed refresh button

--- src/main/java/no/jonasandersen/admin/adapter/in/web/LinodeHxController.java
+++ src/main/java/no/jonasandersen/admin/adapter/in/web/LinodeHxController.java
@@ -3,10 +3,9 @@ package no.jonasandersen.admin.adapter.in.web;
 import io.github.wimdeblauwe.htmx.spring.boot.mvc.HxRequest;
 import java.util.List;
 import java.util.stream.Collectors;
-import no.jonasandersen.admin.adapter.out.websocket.Model;
-import no.jonasandersen.admin.core.minecraft.LinodeVolumeService;
-import no.jonasandersen.admin.core.domain.LinodeVolume;
 import no.jonasandersen.admin.application.LinodeService;
+import no.jonasandersen.admin.core.domain.LinodeVolume;
+import no.jonasandersen.admin.core.minecraft.LinodeVolumeService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -36,16 +35,6 @@ public class LinodeHxController {
   String createInstance() {
     log.info("Returning create instance form");
     return jteHtmlGenerator.generateHtml("linode/create-form");
-  }
-
-  @GetMapping("/instance/refresh")
-  @HxRequest
-  String refreshInstance() {
-
-    List<no.jonasandersen.admin.core.domain.LinodeInstance> instances = linodeService.getInstances();
-    return instances.stream()
-        .map(instance -> jteHtmlGenerator.generateHtml("linode/instance", Model.from(instance)))
-        .collect(Collectors.joining());
   }
 
   @GetMapping("/volumes")
