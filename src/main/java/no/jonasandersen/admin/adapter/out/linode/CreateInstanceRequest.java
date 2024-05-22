890ce9034919ad9083c0b841b6842c2266f9248a
Jonas Bjørge Andersen <dev@jonasandersen.no>
2024-05-22 22:10:02 +0200

    Cleaned up linode out adapter

--- src/main/java/no/jonasandersen/admin/adapter/out/linode/CreateInstanceRequest.java
+++ src/main/java/no/jonasandersen/admin/adapter/out/linode/CreateInstanceRequest.java
@@ -4,10 +4,10 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.List;
 import no.jonasandersen.admin.domain.InstanceDetails;
 
-public record CreateInstanceRequest(String region, String image, String label, String type, List<String> tags,
+record CreateInstanceRequest(String region, String image, String label, String type, List<String> tags,
                                     @JsonProperty("root_pass") String rootPassword, Boolean volume) {
 
-  public static CreateInstanceRequest from(InstanceDetails details) {
+  static CreateInstanceRequest from(InstanceDetails details) {
     return new CreateInstanceRequest(details.region(), details.image(), details.label(), details.type(),
         details.tags(), details.rootPassword().value(), details.volume());
   }
--- src/main/java/no/jonasandersen/admin/adapter/out/linode/Instance.java
+++
@@ -1,7 +0,0 @@
-package no.jonasandersen.admin.adapter.out.linode;
-
-import java.util.Date;
-
-public record Instance(Long id, String label, String ip, String status, Date created) {
-
-}
