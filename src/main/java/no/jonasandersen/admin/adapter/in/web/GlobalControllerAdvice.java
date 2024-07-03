ab1444d1da2e5b241dbab236a34cd1161bbf1011
Jonas Bjørge Andersen <dev@jonasandersen.no>
2024-07-03 21:34:19 +0200

    Fixed active class on navbar

--- src/main/java/no/jonasandersen/admin/adapter/in/web/GlobalControllerAdvice.java
+++ src/main/java/no/jonasandersen/admin/adapter/in/web/GlobalControllerAdvice.java
@@ -1,8 +1,11 @@
 package no.jonasandersen.admin.adapter.in.web;
 
+import jakarta.servlet.http.HttpServletRequest;
 import no.jonasandersen.admin.adapter.UsernameResolver;
 import no.jonasandersen.admin.application.ThemeService;
 import no.jonasandersen.admin.domain.Username;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.info.BuildProperties;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -10,6 +13,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 @ControllerAdvice(basePackages = "no.jonasandersen.admin.adapter.in.web")
 public class GlobalControllerAdvice {
 
+  private static final Logger log = LoggerFactory.getLogger(GlobalControllerAdvice.class);
   private final BuildProperties buildProperties;
   private final ThemeService themeService;
 
@@ -31,5 +35,11 @@ public class GlobalControllerAdvice {
   @ModelAttribute("buildVersion")
   public String getBuildVersion() {
     return buildProperties.getVersion();
+  }
+
+  @ModelAttribute("currentPath")
+  public String getCurrentPath(HttpServletRequest request) {
+    log.info("Request URI: {}", request.getRequestURI());
+    return request.getRequestURI();
   }
 }
