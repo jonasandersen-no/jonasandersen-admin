b10ecc6dda02320b3417878a320c3e549aaecbea
Jonas Bjørge Andersen <dev@jonasandersen.no>
2024-08-03 01:54:53 +0200

    Fix instance of check

--- service/src/main/java/no/jonasandersen/admin/adapter/UsernameResolver.java
+++ service/src/main/java/no/jonasandersen/admin/adapter/UsernameResolver.java
@@ -4,7 +4,7 @@ import no.jonasandersen.admin.domain.Username;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
-import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 
 public class UsernameResolver {
 
@@ -19,7 +19,7 @@ public class UsernameResolver {
     String result = "unknown";
     Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
     if (authentication instanceof OAuth2AuthenticationToken token
-        && token.getPrincipal() instanceof DefaultOidcUser user) {
+        && token.getPrincipal() instanceof OidcUser user) {
       result = user.getEmail();
     }
     return result;
