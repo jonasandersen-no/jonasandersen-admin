60a4f1bc651f49fb96b3a995daf11e76e1492055
Jonas Bjørge Andersen <dev@jonasandersen.no>
2024-08-10 16:15:18 +0200

    Set temporary file read only

--- service/src/main/java/no/jonasandersen/admin/application/ControlCenterProperties.java
+++ service/src/main/java/no/jonasandersen/admin/application/ControlCenterProperties.java
@@ -3,6 +3,7 @@ package no.jonasandersen.admin.application;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Files;
 import no.jonasandersen.admin.domain.SensitiveString;
 
 public record ControlCenterProperties(String username, String privateKeyFilePath, String ip, Integer port) {
@@ -11,10 +12,13 @@ public record ControlCenterProperties(String username, String privateKeyFilePath
       Integer port) {
     File tempFile;
     try {
-      tempFile = File.createTempFile("privateKey", ".pem");
+      tempFile = Files.createTempFile("privateKey", ".pem").toFile();
       try (FileOutputStream fileOutputStream = new FileOutputStream(tempFile)) {
         fileOutputStream.write(privateKeyFile.value().getBytes());
       }
+      tempFile.setReadable(true, true);
+      tempFile.setWritable(false);
+      tempFile.setExecutable(true, true);
 
     } catch (IOException e) {
       throw new RuntimeException(e);
