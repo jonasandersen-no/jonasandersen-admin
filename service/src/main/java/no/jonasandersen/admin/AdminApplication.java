8eabb7c6fa90c37d3e7e7d63acb179556c50f12b
Jonas Bjørge Andersen <dev@jonasandersen.no>
2024-08-19 22:39:56 +0200

    Added db scheduler

--- service/src/main/java/no/jonasandersen/admin/AdminApplication.java
+++ service/src/main/java/no/jonasandersen/admin/AdminApplication.java
@@ -4,13 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
 @EnableCaching
-@EnableScheduling
 @EnableMethodSecurity(securedEnabled = true)
 public class AdminApplication {
 
