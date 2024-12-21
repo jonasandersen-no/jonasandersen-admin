aa47fd9f3abf40b02593b93f42b610f46f590a97
Jonas Bjørge Andersen <dev@jonasandersen.no>
2024-12-21 23:49:15 +0100

    Test new MockMvcTester
    Added exception handling for bad request
    
    Signed-off-by: Jonas Bjørge Andersen <dev@jonasandersen.no>

--- service/src/main/java/no/jonasandersen/admin/adapter/in/web/TemperatureController.java
+++ service/src/main/java/no/jonasandersen/admin/adapter/in/web/TemperatureController.java
@@ -1,11 +1,15 @@
 package no.jonasandersen.admin.adapter.in.web;
 
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import java.util.List;
 import no.jonasandersen.admin.application.port.StoreMeasurement;
 import no.jonasandersen.admin.domain.Measurement;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,6 +38,10 @@ public class TemperatureController {
 
   @PostMapping
   @ResponseStatus(HttpStatus.CREATED)
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "201", description = "Measurement stored"),
+      @ApiResponse(responseCode = "400", description = "Bad request")
+  })
   void catchAll(@RequestBody Device device) {
     log.info("Received request: {}", device);
 
@@ -44,5 +52,11 @@ public class TemperatureController {
         .findFirst().orElseThrow(() -> new IllegalArgumentException("No sensor with tag Humidity"));
 
     storeMeasurement.store(new Measurement(temperature.sensorvalue(), humidity.sensorvalue()));
+  }
+
+  @ExceptionHandler(IllegalArgumentException.class)
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  ProblemDetail handleIllegalArgumentException(IllegalArgumentException e) {
+    return ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, e.getMessage());
   }
 }
