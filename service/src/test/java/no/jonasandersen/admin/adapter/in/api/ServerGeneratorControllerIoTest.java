5194dd57ce07b7554b3b06f364507c3f245be282
Jonas Bjørge Andersen <dev@jonasandersen.no>
2024-12-24 12:13:33 +0100

    Changed to use MockMvcTester

--- service/src/test/java/no/jonasandersen/admin/adapter/in/api/ServerGeneratorControllerIoTest.java
+++ service/src/test/java/no/jonasandersen/admin/adapter/in/api/ServerGeneratorControllerIoTest.java
@@ -1,63 +1,38 @@
 package no.jonasandersen.admin.adapter.in.api;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import no.jonasandersen.admin.application.ServerGenerator;
-import no.jonasandersen.admin.config.WebBasedTest;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.web.servlet.assertj.MockMvcTester;
 
-@WebMvcTest(ServerGeneratorController.class)
-class ServerGeneratorControllerIoTest extends WebBasedTest {
+class ServerGeneratorControllerIoTest {
 
-  @Autowired
-  private MockMvc mockMvc;
+  private final MockMvcTester mockMvc = MockMvcTester.of(
+      new ServerGeneratorController(ServerGenerator.createNull()));
 
   @Test
   void minecraftServerTypeReturns200() throws Exception {
-    mockMvc.perform(post("/api/server-generator").with(csrf())
-            .content("""
-                {
-                "serverType": "MINECRAFT"
-                }""")
-            .contentType("application/json"))
-        .andExpect(status().isOk());
+    mockMvc.post().uri("/api/server-generator").with(csrf())
+        .content("""
+            {
+            "serverType": "MINECRAFT"
+            }""")
+        .contentType("application/json")
+        .assertThat()
+        .hasStatusOk();
   }
 
   @Test
   void defaultServerTypeThrows500() throws Exception {
-    mockMvc.perform(post("/api/server-generator").with(csrf())
-            .content("""
-                {
-                "serverType": "DEFAULT"
-                }""")
-            .contentType("application/json"))
-        .andExpect(status().isInternalServerError());
-  }
-
-//  @Test
-//  void responseBodyContainsUsername() throws Exception {
-//    assertThat(mockMvc.perform(post("/api/server-generator").with(csrf())
-//        .content("""
-//            {
-//            "serverType": "MINECRAFT"
-//            }""")
-//        .contentType("application/json")))
-//        .matches(status().isOk());
-//  }
-
-  @TestConfiguration
-  static class TestConfig {
-
-    @Bean
-    ServerGenerator serverGenerator() {
-      return ServerGenerator.createNull();
-    }
+    mockMvc.post().uri("/api/server-generator").with(csrf())
+        .content("""
+            {
+            "serverType": "DEFAULT"
+            }""")
+        .contentType("application/json")
+        .assertThat()
+        .hasStatus(HttpStatus.INTERNAL_SERVER_ERROR);
   }
 }
