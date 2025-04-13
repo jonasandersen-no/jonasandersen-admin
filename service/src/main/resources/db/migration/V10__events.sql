97f2d632beaf72a057050542a08c14ca9b3ba24e
Jonas Bjørge Andersen <dev@jonasandersen.no>
2025-04-13 12:58:27 +0200

    Remove events sql (never applied in production)
    Signed-off-by: Jonas Bjørge Andersen <dev@jonasandersen.no>

--- service/src/main/resources/db/migration/V10__events.sql
+++
@@ -1,8 +0,0 @@
-create table events
-(
-    aggregate_root_id uuid,
-    recorded_at       timestamp not null,
-    event_id          integer   not null,
-    event_type        text      not null,
-    content           json      not null
-);
