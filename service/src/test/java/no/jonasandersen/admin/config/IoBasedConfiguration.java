e8f8c38b16745c51b69ab5e621a17b77c13d74b9
Jonas Bjørge Andersen <dev@jonasandersen.no>
2025-10-28 21:01:13 +0100

    Fixed up test dependencies

--- service/src/test/java/no/jonasandersen/admin/config/IoBasedConfiguration.java
+++ service/src/test/java/no/jonasandersen/admin/config/IoBasedConfiguration.java
@@ -3,8 +3,8 @@ package no.jonasandersen.admin.config;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
-import org.testcontainers.containers.MongoDBContainer;
-import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.mongodb.MongoDBContainer;
+import org.testcontainers.postgresql.PostgreSQLContainer;
 import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration
@@ -12,8 +12,8 @@ public class IoBasedConfiguration {
 
   @Bean
   @ServiceConnection
-  PostgreSQLContainer<?> postgreSQLContainer() {
-    return new PostgreSQLContainer<>(DockerImageName.parse("postgres:17"))
+  PostgreSQLContainer postgreSQLContainer() {
+    return new PostgreSQLContainer(DockerImageName.parse("postgres:17"))
         .withDatabaseName("admin");
   }
 
