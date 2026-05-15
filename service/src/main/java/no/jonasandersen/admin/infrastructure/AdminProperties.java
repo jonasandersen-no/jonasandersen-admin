bfbb1507e7f71de631658edf6396cb5b18ce468b
Jonas Bjørge Andersen <dev@jonasandersen.no>
2026-05-15 21:54:37 +0200

    Removed unused properties and refactored `AdminProperties` class.

--- service/src/main/java/no/jonasandersen/admin/infrastructure/AdminProperties.java
+++ service/src/main/java/no/jonasandersen/admin/infrastructure/AdminProperties.java
@@ -6,19 +6,12 @@ import java.util.EnumMap;
 import java.util.Map;
 import no.jonasandersen.admin.domain.Feature;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.bind.DefaultValue;
 import org.springframework.validation.annotation.Validated;
 
 @ConfigurationProperties(prefix = "jonasandersen.admin")
 @Validated
 public record AdminProperties(
-    @Valid Minecraft minecraft,
-    @Valid Cloudflare cloudflare,
-    @Valid Linode linode,
-    @NotNull String defaultTheme,
-    Map<Feature, Boolean> features,
-    @Valid Actuator actuator,
-    @Valid Discord discord) {
+    Map<Feature, Boolean> features, @Valid Actuator actuator, @Valid Discord discord) {
 
   public AdminProperties {
 
@@ -26,23 +19,6 @@ public record AdminProperties(
       features = new EnumMap<>(Feature.class);
     }
   }
-
-  @Valid
-  public record Minecraft(
-      @NotNull String username,
-      @NotNull String password,
-      @NotNull @DefaultValue("22") Integer port) {}
-
-  @Valid
-  public record Cloudflare(
-      @NotNull String apiKey, @NotNull String zoneId, @NotNull String dnsRecordId) {}
-
-  @Valid
-  public record Linode(
-      @NotNull String baseUrl,
-      @NotNull String token,
-      @NotNull String rootPassword,
-      @NotNull Long volumeId) {}
 
   @Valid
   public record Actuator(@NotNull String username, @NotNull String password) {}
