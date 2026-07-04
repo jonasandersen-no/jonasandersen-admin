0b45bbcce43c350ac75737f97d3a624111c39f4c
Jonas Bjørge Andersen <dev@jonasandersen.no>
2026-07-04 19:28:26 +0200

    Refactored `SyncService` to initialize `FileExecutor` internally using `AdminProperties`, removed redundant `FileExecutor` bean from `MinecraftConfig`.

--- service/src/main/java/no/jonasandersen/admin/minecraft/MinecraftConfig.java
+++ service/src/main/java/no/jonasandersen/admin/minecraft/MinecraftConfig.java
@@ -3,16 +3,6 @@ package no.jonasandersen.admin.minecraft;
 import com.github.kagkarlsson.scheduler.task.helper.RecurringTask;
 import com.github.kagkarlsson.scheduler.task.helper.Tasks;
 import com.github.kagkarlsson.scheduler.task.schedule.FixedDelay;
-import com.jcraft.jsch.JSchException;
-import no.jonasandersen.admin.domain.ConnectionInfo;
-import no.jonasandersen.admin.domain.Feature;
-import no.jonasandersen.admin.domain.Hostname;
-import no.jonasandersen.admin.domain.PasswordConnectionInfo;
-import no.jonasandersen.admin.domain.SensitiveString;
-import no.jonasandersen.admin.infrastructure.AdminProperties;
-import no.jonasandersen.admin.infrastructure.AdminProperties.Minecraft.Andersen;
-import no.jonasandersen.admin.infrastructure.Features;
-import no.jonasandersen.admin.ssh.FileExecutor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -26,21 +16,5 @@ class MinecraftConfig {
             (taskInstance, executionContext) -> {
               syncTask.executeSyncTask(executionContext.getExecution().getExecutionTime());
             });
-  }
-
-  @Bean(destroyMethod = "cleanup")
-  FileExecutor minecraftFileExecutor(AdminProperties properties) throws JSchException {
-    Andersen andersen = properties.minecraft().andersen();
-
-    FileExecutor fileExecutor = FileExecutor.create();
-
-    ConnectionInfo info =
-        new PasswordConnectionInfo(
-            andersen.username(),
-            SensitiveString.of(andersen.password()),
-            new Hostname(andersen.hostname()),
-            22);
-    fileExecutor.setup(info);
-    return fileExecutor;
   }
 }
--- service/src/main/java/no/jonasandersen/admin/minecraft/SyncService.java
+++ service/src/main/java/no/jonasandersen/admin/minecraft/SyncService.java
@@ -3,6 +3,12 @@ package no.jonasandersen.admin.minecraft;
 import com.jcraft.jsch.JSchException;
 import java.io.IOException;
 import java.util.stream.Stream;
+import no.jonasandersen.admin.domain.ConnectionInfo;
+import no.jonasandersen.admin.domain.Hostname;
+import no.jonasandersen.admin.domain.PasswordConnectionInfo;
+import no.jonasandersen.admin.domain.SensitiveString;
+import no.jonasandersen.admin.infrastructure.AdminProperties;
+import no.jonasandersen.admin.infrastructure.AdminProperties.Minecraft.Andersen;
 import no.jonasandersen.admin.ssh.FileExecutor;
 import org.springframework.stereotype.Service;
 
@@ -11,13 +17,27 @@ class SyncService {
 
   public static final String COMMAND =
       "rsync --delete -av --exclude-from='exclude-andersen.txt' /root/andersen/ gollien@mira2:/mnt/nas/public/backups/minecraft/andersen";
-  private final FileExecutor fileExecutor;
 
-  public SyncService(FileExecutor minecraftFileExecutor) {
-    this.fileExecutor = minecraftFileExecutor;
+  private final AdminProperties properties;
+
+  public SyncService(AdminProperties properties) {
+    this.properties = properties;
   }
 
   public void sync() throws IOException, InterruptedException, JSchException {
+    Andersen andersen = properties.minecraft().andersen();
+
+    FileExecutor fileExecutor = FileExecutor.create();
+
+    ConnectionInfo info =
+        new PasswordConnectionInfo(
+            andersen.username(),
+            SensitiveString.of(andersen.password()),
+            new Hostname(andersen.hostname()),
+            22);
+
+    fileExecutor.setup(info);
     fileExecutor.runLines(Stream.of(COMMAND));
+    fileExecutor.cleanup();
   }
 }
